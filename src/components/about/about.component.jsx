c8d19f9091c1fc595aa1a109bdb83b67f580bcf1
SumanIslam <sumanislam@gmail.com>
2021-11-15 02:23:58 +0600

    add Fade animation in about component

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// dependencies
+import Fade from 'react-reveal/Fade';
 
 // components
 import SectionTitle from "../section-title/section-title.component";
@@ -14,31 +17,49 @@ import {
   AboutContentWrapperInfo,
 } from "./about.style";
 
-const About = ({ aboutInfo }) => (
-  <AboutContainer id="about">
-    <div class="container">
-      <SectionTitle title="About Me"/>
-      <AboutContentWrapper className="row">
-        <div className="col-md-4 col-sm-12">
-          <AboutContentWrapperImage>
-            <img className="img-fluid rounded shadow-lg" height="auto" width="300px" src={profile} alt="Profile" />
-          </AboutContentWrapperImage>
-        </div>
-        <div class="col-md-8 col-sm-12">
-          <AboutContentWrapperInfo>
-            {
-              aboutInfo.map(info => (
-                <AboutInfo content={info} />
-              ))
-            }
-            <Button text="Resume" link="https://drive.google.com/file/d/1kV1WVnMWCvmQrfRZRjFKOdXbZCvwfjRn/view?usp=sharing"/>
-          </AboutContentWrapperInfo>
-        </div>
-      </AboutContentWrapper>
-    </div>
-  </AboutContainer>
-  
-);
+const About = ({ aboutInfo }) => {
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <AboutContainer id="about">
+      <div class="container">
+        <SectionTitle title="About Me"/>
+        <AboutContentWrapper className="row">
+          <div className="col-md-4 col-sm-12">
+            <Fade bottom duration={1000} delay={600} distance="30px">
+              <AboutContentWrapperImage>
+                <img className="img-fluid rounded shadow-lg" height="auto" width="300px" src={profile} alt="Profile" />
+              </AboutContentWrapperImage>
+            </Fade>
+          </div>
+          <div class="col-md-8 col-sm-12">
+             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <AboutContentWrapperInfo>
+                {
+                  aboutInfo.map(info => (
+                    <AboutInfo content={info} />
+                  ))
+                }
+                <Button text="Resume" link="https://drive.google.com/file/d/1kV1WVnMWCvmQrfRZRjFKOdXbZCvwfjRn/view?usp=sharing"/>
+              </AboutContentWrapperInfo>
+            </Fade>
+          </div>
+        </AboutContentWrapper>
+      </div>
+    </AboutContainer> 
+  )
+};
 
 About.defaultProps = {
   aboutInfo: [
