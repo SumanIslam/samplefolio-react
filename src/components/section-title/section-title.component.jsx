46d7d6a2bb66daaa205ae302c82403829541b27f
SumanIslam <sumanislam@gmail.com>
2021-11-15 02:16:00 +0600

    add Fade animation in section-title component

--- src/components/section-title/section-title.component.jsx
+++ src/components/section-title/section-title.component.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+
+// dependencies
+import Fade from 'react-reveal/Fade';
+
+// styles
 import { TitleContainer } from "./section-title.style";
 
 const SectionTitle = ({title}) => (
-  <TitleContainer>{title}</TitleContainer>
+  <Fade bottom duration={1000} delay={300} distance="0px">
+    <TitleContainer>{title}</TitleContainer>
+  </Fade>
 );
 
 export default SectionTitle;
