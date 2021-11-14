0575d9f836fc9e364fdcf587a578e7ef07bc50b7
SumanIslam <sumanislam@gmail.com>
2021-11-15 01:38:53 +0600

    updat some variable pixel and remove unnecessary comments

--- src/styles/variable.js
+++ src/styles/variable.js
@@ -6,22 +6,8 @@ export const whiteColor = '#fff';
 export const darkColor = '#333333';
 export const darkBlueText = '#272341';
 
+
 // font size
-export const defaultFontSize = '1.6rem';
+export const defaultFontSize = '1rem';
 export const bigFontSize = '2.5rem';
-export const midFontSize = '2rem';
-
-// // COLORS
-// $primary-color: #02aab0;
-// $secondary-color: #00cdac;
-
-// $white-color: #fff;
-
-// $dark-grey: #333333;
-
-// $dark-blue-text: #272341; // For Headings
-
-// // FONT SIZE
-// $default-font-size: 1.6rem;
-// $big-font-size: 4rem;
-// $mid-font-size: 2.5rem;
+export const midFontSize = '1.56rem';
