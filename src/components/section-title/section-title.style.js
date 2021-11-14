1dc7dc869573ef98fb399b9f87e78e4fa5fa08ea
SumanIslam <sumanislam@gmail.com>
2021-11-15 01:41:52 +0600

    change margin  and add media query for phone in section-title component

--- src/components/section-title/section-title.style.js
+++ src/components/section-title/section-title.style.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 import { bigFontSize } from "../../styles/variable";
+import breakpoint from "../../styles/breakpoint";
 
 export const TitleContainer = styled.h1`
   margin: 0;
-  margin-bottom: 4.5rem;
+  margin-bottom: 2.81rem;
   font-size: ${bigFontSize};
   font-weight: bold;
   text-transform: uppercase;
   text-align: center;
+
+  @media screen and ${breakpoint.device.phone} {
+    font-size: 1.75rem;
+  }
 `
