fe8ef990d01308d4e6ac014c3aad0c2d3194e242
Sumna Islam <sumanislam1416@gmail.com>
2021-12-09 23:18:59 +0600

    make education section responsive

--- src/components/education/education.style.js
+++ src/components/education/education.style.js
@@ -1,15 +1,32 @@
 import styled from "styled-components";
-import { whiteColor } from "../../styles/variable";
+import { whiteColor } from "../../utils/variable";
+import breakpoint from "../../utils/breakpoint";
 
 export const EducationSectionContainer = styled.section`
   background-color: ${whiteColor};
-  padding: 2rem 15rem 4rem 15rem;
+  padding: 2rem 0 4rem;
 `;
 
 export const TimelineContainer = styled.div`
   height: 100%;
   width: 100;
-  margin-left: 10rem;
+  margin-left: 18rem;
+
+  @media screen and ${breakpoint.device.tab_land} {
+    margin-left: 10rem;
+  }
+  @media screen and ${breakpoint.device.tab_port} {
+    margin-left: 5rem;
+  }
+  @media screen and ${breakpoint.device.tab_port_sm} {
+    margin-left: 3rem;
+  }
+  @media screen and ${breakpoint.device.phone} {
+    margin-left: 1rem;
+  }
+  @media screen and ${breakpoint.device.phone_sm} {
+    margin-left: 0.5rem;
+  }
 
   h3 {
     font-size: 1.6rem;
