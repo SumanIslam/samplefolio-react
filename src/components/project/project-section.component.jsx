1e63578eb6fc041fb7255ade42ea9d860545c68a
Sumna Islam <sumanislam1416@gmail.com>
2022-01-02 23:52:42 +0600

    add nasa-mission-control and crwn-clothing projects details in project section

--- src/components/project/project-section.component.jsx
+++ src/components/project/project-section.component.jsx
@@ -1,74 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // dependencies
 import { v4 as uuidv4 } from 'uuid';
-import { Link } from "react-router-dom";
-// components
-import ProjectMainContent from "./project-main-content/project-main-content";
+import { ButtonContainer } from "../button/button.style";
 import SectionTitle from '../section-title/section-title.component';
 
-// images
-import CrawnClothing from "../../assets/crwn-clothing.png";
-import MoviesHome from '../../assets/movieshome.png';
+// components
+import ProjectMainContent from "./project-main-content/project-main-content";
+
+// data
+import { projectsData } from "./project-data";
 
 // styles
-import { ProjectSectionContainer, ProjectContentWrapper, ProjectButton } from "./project-section.style";
-import { ButtonStyle, ButtonContainer } from "../button/button.style";
+import { ProjectButton, ProjectContentWrapper, ProjectSectionContainer } from "./project-section.style";
 
-const Project = ({ projects }) => {
-  return (
-    <ProjectSectionContainer>
-      <div className="container">
-        <ProjectContentWrapper>
-          <SectionTitle title="Projects" />
-          {
-            projects.map(project => (
-              <ProjectMainContent key={uuidv4()} project={project} />
-            ))
-          }
-        </ProjectContentWrapper>
-        <ProjectButton>
-            <ButtonContainer primary="primary">
-              <span>
-                <Link to="projects">See More Projects</Link>
-              </span>
-            </ButtonContainer>
-        </ProjectButton>
-      </div>
-    </ProjectSectionContainer>
-  )
-};
-
-Project.defaultProps = {
-  projects: [
-    {
-      title: "Crwn Clothing",
-      description1: "Crwn Clothing is a fictional E-commerce Wep Application",
-      description2: "Omnifood a fictional restaurant website where customer can find healthy foods. Here you will get 24/7 service. It has various menu items. You can order online too.",
-      stack: ["React","Redux","Styled-Component","Node","Firebase", "Stripe"],
-      liveLink:"https://crwn-live-preview.herokuapp.com/",
-      sourceLink: "https://github.com/SumanIslam/omnifood",
-      imageSource: CrawnClothing,
-    },
-    {
-      title: "Crwn Clothing",
-      description1: "Crwn Clothing is a fictional E-commerce Wep Application",
-      description2: "Omnifood a fictional restaurant website where customer can find healthy foods. Here you will get 24/7 service. It has various menu items. You can order online too.",
-      stack: ["React","Redux","Styled-Component","Node","Firebase"],
-      liveLink:"https://crwn-live-preview.herokuapp.com/",
-      sourceLink: "https://github.com/SumanIslam/omnifood",
-      imageSource: MoviesHome,
-    },
-    {
-      title: "Crwn Clothing",
-      description1: "Crwn Clothing is a fictional E-commerce Wep Application",
-      description2: "Omnifood a fictional restaurant website where customer can find healthy foods. Here you will get 24/7 service. It has various menu items. You can order online too.",
-      stack: ["React","Redux","Styled-Component","Node","Firebase"],
-      liveLink:"https://crwn-live-preview.herokuapp.com/",
-      sourceLink: "https://github.com/SumanIslam/omnifood",
-      imageSource: MoviesHome,
-    },
-  ]
-};
+const Project = () => (
+  <ProjectSectionContainer>
+    <div className="container">
+      <ProjectContentWrapper>
+        <SectionTitle title="Projects" />
+        {
+          projectsData.map(project => (
+            <ProjectMainContent key={uuidv4()} project={project} />
+          ))
+        }
+      </ProjectContentWrapper>
+      <ProjectButton>
+          <ButtonContainer primary="primary">
+            <span>
+              <Link to="projects">See More Projects</Link>
+            </span>
+          </ButtonContainer>
+      </ProjectButton>
+    </div>
+  </ProjectSectionContainer>
+);
 
 export default Project;
