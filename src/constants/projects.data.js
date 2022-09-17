52b38602805a694b2e186e7c051c4f96326b5772
Suman Islam <sumanislam1416@gmail.com>
2022-09-17 16:41:06 +0600

    add infinity scroll app

--- src/constants/projects.data.js
+++ src/constants/projects.data.js
@@ -7,6 +7,7 @@ import Weatherin from '../assets/weatherin.png';
 import Diceplay from '../assets/diceplay.png';
 import MovieTicketBooking from '../assets/movie-ticket-booking.png';
 import LightsOut from '../assets/lightsout.png';
+import InfinityScroll from '../assets/infinity.png';
 
 export const projectsData = [
 	{
@@ -96,7 +97,14 @@ export const projectsData = [
 		description1: 'Weatherin is a Front-End Web Application.',
 		description2:
 			'This is a weather web application where you can get the weather details. You can search for cities or countries to see the weather condition of that place. You can also see whether it is day or night at that moment and whether it is rainy, sunny or cloudy. You can also see the temperature in celsius.',
-		stack: ['HTML', 'CSS', 'Bootstrap', 'Vanila JavaScript', 'AccuWeather API'],
+		stack: [
+			'HTML',
+			'CSS',
+			'Bootstrap',
+			'Vanila JavaScript',
+			'AccuWeather API',
+			'Local Storage',
+		],
 		liveLink: 'https://weatherin.vercel.app/',
 		sourceLink: 'https://github.com/SumanIslam/Weather-app-1',
 		imageSource: Weatherin,
@@ -118,7 +126,7 @@ export const projectsData = [
 		description1: 'Movie Ticket Booking is a Front-End Web Application.',
 		description2:
 			'This is a web application where you can book the movie ticket. You can book ticket for different movies for different price. You can see the Available and occupied seats. When you select the seat you will see the selected seat in different color that helps you distinguish selected and other seats. Besides, you can also see the number of seats you booked and the total price of those seats.',
-		stack: ['React', 'CSS', 'Context API', 'React Hooks'],
+		stack: ['React', 'CSS', 'Context API', 'React Hooks', 'Local Storage'],
 		liveLink: 'https://movieticketbooking.vercel.app/',
 		sourceLink: 'https://github.com/SumanIslam/movie-ticket-booking',
 		imageSource: MovieTicketBooking,
@@ -133,5 +141,16 @@ export const projectsData = [
 		liveLink: 'https://lightsoutgame-theta.vercel.app/',
 		sourceLink: 'https://github.com/SumanIslam/lights-out-game-react',
 		imageSource: LightsOut,
+	},
+	{
+		id: 10,
+		title: 'Infinity Scroll App',
+		description1: 'Infinity Scroll is a Front-End Web Game Application.',
+		description2:
+			"This is a web application where you can see unlimited random photos. Unsplash API has been used to fetch the photos and display. You find photos and photos by scrolling. It's a never ending cycle. When you will get to the bottom of screen by scrolling another API call will be made and fetch random photos.",
+		stack: ['HTML', 'CSS', 'JavaScript', 'Unsplash API'],
+		liveLink: 'https://infinityscroll-sigma.vercel.app/',
+		sourceLink: 'https://github.com/SumanIslam/infinity-scroll',
+		imageSource: InfinityScroll,
 	},
 ];
